87bff9e04772a90b318dabbd75241a0a4f32e64a
MWarCZ <miroslav@tenvalka.eu>
2024-06-16 18:17:44 +0200

    Dokoncena funkce cropImage vyuzivajici GD

--- src/Image/ImageGD.php
+++ src/Image/ImageGD.php
@@ -3,6 +3,8 @@
 //* File: src/Image/ImageGD.php
 namespace MWarCZ\Segami\Image;
 
+use MWarCZ\Segami\Exception\UnsupportedImageExtensionException;
+
 class ImageGD implements Image {
   protected $fn_imagecreatefrom;
   protected $fn_image;
@@ -23,7 +25,7 @@ class ImageGD implements Image {
       'png' => 'imagecreatefrompng',
       'webp' => 'imagecreatefromwebp',
     ];
-    $this->fn_imagecreatefrom = [
+    $this->fn_image = [
       'avif' => 'imageavif',
       'bmp' => 'imagebmp',
       'gif' => 'imagegif',
@@ -43,7 +45,11 @@ class ImageGD implements Image {
 
   function read($srcFile) {
     $tmp = explode('.', $srcFile);
-    $this->src_extension = strtolower(end($tmp));
+
+    $extension_tmp = strtolower(end($tmp));
+    if (!isset($this->fn_imagecreatefrom[$extension_tmp]))
+      throw new UnsupportedImageExtensionException($extension_tmp);
+    $this->src_extension = $this->dst_extension = $extension_tmp;
     $this->img = $this->fn_imagecreatefrom[$this->src_extension]($srcFile);
     return $this;
   }
@@ -58,13 +64,18 @@ class ImageGD implements Image {
   }
 
   function setFormat($extension) {
-    $this->src_extension = strtolower(end($extension));
+    $extension_tmp = strtolower($extension);
+    if (!isset($this->fn_image[$extension_tmp]))
+      throw new UnsupportedImageExtensionException($extension_tmp);
+    $this->dst_extension = $extension_tmp;
     return $this;
   }
 
   function resizeFill($width, $height) {
-    // imagecopyresampled();
-    $this->img = imagescale($this->img, $width, $height);
+    $img = imagescale($this->img, $width, $height);
+    imagedestroy($this->img);
+    $this->img = $img;
+    imagedestroy($img);
     return $this;
   }
 
@@ -76,7 +87,34 @@ class ImageGD implements Image {
     return $this;
   }
 
-  function cropImage($width, $height, $s_x, $s_y) {
+  function cropImage($width, $height, $s_x = 'center', $s_y = 'center') {
+    $w = imagesx($this->img);
+    $h = imagesy($this->img);
+    /////////////////////////////////////////////
+    // Výpočet X
+    $x = ($w - $width) / 2; // Default is center
+    if (is_numeric($s_x))
+      $x = intval($s_x);
+    elseif ($s_x == 'left')
+      $x = 0;
+    elseif ($s_x == 'right')
+      $x = ($w - $width);
+    /////////////////////////////////////////////
+    // Výpočet Y
+    $y = ($h - $height) / 2; // Default is center
+    if (is_numeric($s_y))
+      $y = intval($s_y);
+    elseif ($s_y == 'top')
+      $y = 0;
+    elseif ($s_y == 'bottom')
+      $y = ($h - $height);
+    /////////////////////////////////////////////
+    // Provedení ořezu
+    $img = imagecrop($this->img, ['x' => $x, 'y' => $y, 'width' => $width, 'height' => $height]);
+    /////////////////////////////////////////////
+    // Úklid
+    imagedestroy($this->img);
+    $this->img = $img;
     return $this;
   }
 
